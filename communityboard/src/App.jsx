e949f4350eb0ef986088978dbae3a5bb32810680
tybeller <ty.c.beller@gmail.com>
2023-09-23 10:49:24 -0400

    added more cards

--- communityboard/src/App.jsx
+++ communityboard/src/App.jsx
@@ -6,6 +6,11 @@ import olenoImg from './assets/oleno.jpg';
 import paynesImg from './assets/paynesprarie.jpg';
 import rainbowImg from './assets/rainbow springs.jpg';
 import silverImg from './assets/silversprings.jpg';
+import fanningImg from './assets/fanning.jpg';
+import ichetukneeImg from './assets/ichetuknee.jpg';
+import manateeImg from './assets/manateesprings.jpg';
+import mikeroesImg from './assets/mikeroes.jpg';
+import sanfelascoImg from './assets/sanfelasco.jpg';
 
 const App = () => {
 
@@ -42,6 +47,37 @@ const App = () => {
           title="Silver Springs State Park"
           genre="5656 E Silver Springs Blvd, Silver Springs, FL 34488"
           buttonHref="https://www.silversprings.com/"
+        />
+
+        <Card
+          imgSrc={fanningImg}
+          title="Fanning Springs State Park"
+          genre="18020 Northwest, US-19, Fanning Springs, FL 32693"
+          buttonHref="https://www.floridastateparks.org/park/Fanning-Springs"
+        />
+        <Card
+          imgSrc={ichetukneeImg}
+          title="Ichetucknee Springs State Park"
+          genre="12087 Southwest, US-27, Fort White, FL 32038"
+          buttonHref="https://www.floridastateparks.org/parks-and-trails/ichetucknee-springs-state-park"
+        />
+        <Card
+          imgSrc={manateeImg}
+          title="Manatee Springs State Park"
+          genre="11650 NW 115th St, Chiefland, FL 32626"
+          buttonHref="https://www.floridastateparks.org/park/manatee-springs"
+        />
+        <Card
+          imgSrc={mikeroesImg}
+          title="Mike Roess Gold Head Branch State Park"
+          genre="6239 State Rd 21, Keystone Heights, FL 32656"
+          buttonHref="https://www.floridastateparks.org/park/Mike-Roess"
+        />
+        <Card
+          imgSrc={sanfelascoImg}
+          title="San Felasco Hammock Preserve State Park"
+          genre="11101 Millhopper Rd, Gainesville, FL 32653"
+          buttonHref="https://www.floridastateparks.org/park/San-Felasco-Hammock"
         />
       </div>
     </div>
